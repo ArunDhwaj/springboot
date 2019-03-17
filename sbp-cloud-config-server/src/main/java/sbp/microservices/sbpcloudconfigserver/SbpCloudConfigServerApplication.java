9669b46c7c4133e66458ffd5fec62f548d59535f
sbp <sbp.arun.01@gmail.com>
2019-03-17 14:20:50 +0530

    cloud server code added

---
+++ sbp-cloud-config-server/src/main/java/sbp/microservices/sbpcloudconfigserver/SbpCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package sbp.microservices.sbpcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbpCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbpCloudConfigServerApplication.class, args);
+	}
+
+}

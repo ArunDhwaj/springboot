390921fcd3ad337af0ed6913f9709e0dbf43d56c
sbp <sbp.arun.01@gmail.com>
2019-03-17 12:54:27 +0530

    microservices code added

---
+++ sbp-limits-service/src/main/java/sbp/microservices/sbplimitsservice/SbpLimitsServiceApplication.java
@@ -0,0 +1,13 @@
+package sbp.microservices.sbplimitsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbpLimitsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbpLimitsServiceApplication.class, args);
+	}
+
+}
